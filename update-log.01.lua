f0aa03ceb2850e18cf13ba7ada550e36d8a09ed9
ecez <93024949+ecez-hk@users.noreply.github.com>
2021-10-25 20:52:48 +0800

    Create update-log.01.lua

---
+++ update-log.01.lua
@@ -0,0 +1,17 @@
+-- Update-Log Author: ecez
+checktoken = ecez61886
+if checktoken == ecez61886 then _G.token = true
+  else _G.token = false
+while _G.token do
+  wait()
+  print("Verified Token Confirmed")
+    end
+  
+UPDATE LOG v.01.
+DISCLAIMER: Please do not use this piece of material for commercial usage or for commercial profit. This piece of information falls into the commercial fair use category
+and should not be used for **ANY** commercial purposes.
+    
+    
+    -- Test Update Log v.01
+    New bot arriving soon :D
+    
